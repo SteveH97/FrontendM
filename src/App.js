f8338486aa1a874af394931fe2af7f1d98befcc9
steven hernandez <stevenhernandez@nevada.unr.edu>
2022-08-19 23:14:08 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { HashRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Home from './Home';
 import ProductPreviewCard from './ProductPreviewCard/ProductPreviewCard';
@@ -36,7 +36,7 @@ function App() {
   }
 
   return (
-    <HashRouter>
+    <BrowserRouter>
       <div className="App">
 
         {(  menu === false ?
@@ -65,7 +65,7 @@ function App() {
         </Routes>
         
       </div>
-    </HashRouter>
+    </BrowserRouter>
     
   );
 }
